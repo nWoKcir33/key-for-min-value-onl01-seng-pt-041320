b9055be61d2858cda48c28d30cf04ebebb67eb36
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-10 14:17:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
 name_hash.collect do |name, points|
-  
+  if points < lowest_value
+      lowest_value = points
+      lowest_key = name
+    end
 end
